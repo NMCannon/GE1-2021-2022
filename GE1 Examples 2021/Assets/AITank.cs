01da0eabd4abd199691748fc4934c947456dfcb9
Noel Cannon <C17308153@mydit.ie>
2021-10-08 17:28:50 +0100

    lab3 attempt

--- GE1 Examples 2021/Assets/AITank.cs
+++ GE1 Examples 2021/Assets/AITank.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
 public class AITank : MonoBehaviour {
 
     public int loops = 10;
@@ -10,8 +11,9 @@ public class AITank : MonoBehaviour {
     public int numWaypoints = 5;
     public int current = 0;
     List<Vector3> waypoints = new List<Vector3>();
-    public float speed = 10;
-    public Transform player;    
+    public float speed = 5;
+    public Transform player;
+    public int count = 0;
 
     public void OnDrawGizmos()
     {
@@ -29,8 +31,10 @@ public class AITank : MonoBehaviour {
         }
     }
 
+
     // Use this for initialization
-    void Awake () {
+    void Awake()
+    {
         // Task 2
         // Put code here to calculate the waypoints in a loop and 
         // Add them to the waypoints List
@@ -44,10 +48,11 @@ public class AITank : MonoBehaviour {
             {
                 float angle = theta * j;
                 float x = Mathf.Sin(angle) * radius * 3;
-                float y = Mathf.Cos(angle) * radius * 3;
-                waypoints.Add(new Vector3(x, y, 0));
+                float z = Mathf.Cos(angle) * radius * 3;
+                waypoints.Add(new Vector3(x, 0.5f, z));
             }
         }
+        
     }
 
     // Update is called once per frame
@@ -55,17 +60,27 @@ public class AITank : MonoBehaviour {
         // Task 3
         // Put code here to move the tank towards the next waypoint
         // When the tank reaches a waypoint you should advance to the next one
-        for(int z=0; z<10;z++)
-        {
-            transform.Translate(waypoints[z] * 10 * Time.deltaTime);
-        }
+        
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[count], speed * Time.deltaTime);
+        StartCoroutine(waiter());
         
 
         // Task 4
         // Put code here to check if the player is in front of or behine the tank
+        // float dot = Vector3.Dot(transform.forward, player.position);
+        //float theta2 = Mathf.Acos(dot) * Mathf.Rad2Deg;
+        //Debug.Log((dot > 0) ? "in front" : "behind");
         // Task 5
         // Put code here to calculate if the player is inside the field of view and in range
         // You can print stuff to the screen using:
         GameManager.Log("Hello from th AI tank");
+    }
+
+    IEnumerator waiter()
+    {
+        //Wait for 6 seconds
+        yield return new WaitForSeconds(6);
+        Debug.Log(waypoints[count]);
+        count++;
     }
 }
